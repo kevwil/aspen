dec8468b3462cd4b2148191ea11b5dfd00ea244e
Kevin Williams <kevwil@gmail.com>
2009-03-27 08:24:47 -0600

    ignore IDE files, update spec_helper for jruby dev

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,7 @@
+require 'java'
+require 'rubygems'
+gem 'rspec'
+require 'spec'
 
 require File.expand_path(
     File.join(File.dirname(__FILE__), %w[.. lib aspen]))
@@ -8,7 +12,7 @@ Spec::Runner.configure do |config|
   # RSpec uses it's own mocking framework by default. If you prefer to
   # use mocha, flexmock or RR, uncomment the appropriate line:
   #
-  # config.mock_with :mocha
+  config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
 end
