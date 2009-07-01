076da72023853972935f2913059f2b84f37b1a85
Kevin Williams <kevwil@gmail.com>
2009-07-01 15:10:16 -0600

    go back to old 'Bones' way of loading - would love to figure out how that combines with the 'autoload' thing that Thin does

--- lib/aspen.rb
+++ lib/aspen.rb
@@ -1,85 +1,61 @@
-require 'java'
-begin
-  $LOAD_PATH << libpath unless $LOAD_PATH.include? libpath
-  require 'netty-3.1.0.CR1.jar'
-rescue LoadError
-  puts "failed to load netty - cannot continue"
-end
-
-require 'logging'
-require 'rack'
-
 module Aspen
-  
-  autoload :Logging, 'aspen/logging'
-  autoload :Server, 'aspen/server'
-  
-  module Rack
-    
-    autoload :Handler, 'aspen/rack/handler'
-    
-  end
-  
-end
 
-# module Aspen
-# 
-#   # :stopdoc:
-#   VERSION = '1.0.0'
-#   LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
-#   PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
-#   # :startdoc:
-# 
-#   # Returns the version string for the library.
-#   #
-#   def self.version
-#     VERSION
-#   end
-# 
-#   # Returns the library path for the module. If any arguments are given,
-#   # they will be joined to the end of the libray path using
-#   # <tt>File.join</tt>.
-#   #
-#   def self.libpath( *args )
-#     args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
-#   end
-# 
-#   # Returns the lpath for the module. If any arguments are given,
-#   # they will be joined to the end of the path using
-#   # <tt>File.join</tt>.
-#   #
-#   def self.path( *args )
-#     args.empty? ? PATH : ::File.join(PATH, args.flatten)
-#   end
-# 
-#   # Utility method used to require all files ending in .rb that lie in the
-#   # directory below this file that has the same name as the filename passed
-#   # in. Optionally, a specific _directory_ name can be passed in such that
-#   # the _filename_ does not have to be equivalent to the directory.
-#   #
-#   def self.require_all_libs_relative_to( fname, dir = nil )
-#     dir ||= ::File.basename(fname, '.*')
-#     search_me = ::File.expand_path(
-#         ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
-# 
-#     Dir.glob(search_me).sort.each {|rb| require rb}
-#   end
-# 
-#   def self.require_java_natives
-#     begin
-#       require 'java'
-#       $LOAD_PATH << libpath unless $LOAD_PATH.include? libpath
-#       # require 'aspenj.jar'
-#       require 'netty-3.1.0.CR1.jar'
-#     rescue LoadError
-#       # puts "aspen lib failed to load aspenj.jar"
-#       puts "failed to load netty - cannot continue"
-#     end
-#   end
-# 
-# end  # module Aspen
-# 
-# Aspen.require_all_libs_relative_to(__FILE__)
-# Aspen.require_java_natives
-# 
-# # EOF
+  # :stopdoc:
+  VERSION = '1.0.0'
+  LIBPATH = ::File.expand_path(::File.dirname(__FILE__)) + ::File::SEPARATOR
+  PATH = ::File.dirname(LIBPATH) + ::File::SEPARATOR
+  # :startdoc:
+
+  # Returns the version string for the library.
+  #
+  def self.version
+    VERSION
+  end
+
+  # Returns the library path for the module. If any arguments are given,
+  # they will be joined to the end of the libray path using
+  # <tt>File.join</tt>.
+  #
+  def self.libpath( *args )
+    args.empty? ? LIBPATH : ::File.join(LIBPATH, args.flatten)
+  end
+
+  # Returns the lpath for the module. If any arguments are given,
+  # they will be joined to the end of the path using
+  # <tt>File.join</tt>.
+  #
+  def self.path( *args )
+    args.empty? ? PATH : ::File.join(PATH, args.flatten)
+  end
+
+  # Utility method used to require all files ending in .rb that lie in the
+  # directory below this file that has the same name as the filename passed
+  # in. Optionally, a specific _directory_ name can be passed in such that
+  # the _filename_ does not have to be equivalent to the directory.
+  #
+  def self.require_all_libs_relative_to( fname, dir = nil )
+    dir ||= ::File.basename(fname, '.*')
+    search_me = ::File.expand_path(
+        ::File.join(::File.dirname(fname), dir, '**', '*.rb'))
+
+    Dir.glob(search_me).sort.each {|rb| require rb}
+  end
+
+  def self.require_java_natives
+    begin
+      require 'java'
+      $LOAD_PATH << libpath unless $LOAD_PATH.include? libpath
+      # require 'aspenj.jar'
+      require 'netty-3.1.0.CR1.jar'
+    rescue LoadError
+      # puts "aspen lib failed to load aspenj.jar"
+      puts "failed to load netty - cannot continue"
+    end
+  end
+
+end  # module Aspen
+
+Aspen.require_java_natives
+Aspen.require_all_libs_relative_to(__FILE__)
+
+# EOF
