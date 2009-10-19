4efd408b1a25cd3adb8e0297b33a93baaa0b271b
Kevin Williams <kevwil@gmail.com>
2009-10-18 21:47:24 -0600

    test broken, trying to fix it, don't know how quite yet

--- javalib/src/test/java/com/github/kevwil/aspen/RackServerHandlerTest.java
+++ javalib/src/test/java/com/github/kevwil/aspen/RackServerHandlerTest.java
@@ -1,12 +1,13 @@
 package com.github.kevwil.aspen;
 
-import java.net.InetSocketAddress;
 import org.jboss.netty.channel.*;
 import org.jboss.netty.handler.codec.http.*;
-import org.jruby.*;
+import org.jruby.Ruby;
 import org.jruby.runtime.builtin.IRubyObject;
-import org.junit.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+import java.net.InetSocketAddress;
 
 /**
  *
@@ -17,17 +18,24 @@ public class RackServerHandlerTest
     @Test
     public void testRequestGivenToRackConverter() throws Exception
     {
-        IRubyObject app = Ruby.getGlobalRuntime().getProc();
+        IRubyObject app = createProc(200, "Hello World");
         RackServerHandler handler = new RackServerHandler(app);
         /* set up args for callback */
         ChannelPipeline pipeline = new DefaultChannelPipeline();
+        pipeline.addLast( "rack", handler );
         ChannelHandlerContext ctx = pipeline.getContext(handler);
         HttpRequest r = new DefaultHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/");
         MessageEvent e = new UpstreamMessageEvent(
-                pipeline.getChannel(), r, new InetSocketAddress(54321));
+                ctx.getChannel(), r, new InetSocketAddress(54321));
 
         /* make callback call */
         handler.messageReceived(ctx, e);
         assertTrue(true);
+    }
+
+    private IRubyObject createProc( final int status, final String body )
+    {
+        Ruby runtime = Ruby.getGlobalRuntime();
+        return runtime.evalScriptlet( "def call(env); [" + status + ", {}, \"" + body + "\"]; end" );
     }
 }
