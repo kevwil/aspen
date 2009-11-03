ed61cb068f24cc743da17f6a3223b6b51546c55d
Kevin Williams <kevwil@gmail.com>
2009-11-02 21:08:15 -0700

    add test using combined jar; need to put into normal unit test syntax

---
+++ test_java_from_ruby.rb
@@ -0,0 +1,17 @@
+require 'java'
+require 'lib/aspenj.jar'
+import com.github.kevwil.aspen.AspenServer
+
+app = lambda { |env| [200, {'Content-Type'=>'text/html'}, 'Hello World'] }
+server = AspenServer.new('localhost',8080,app)
+puts "starting Aspen server ..."
+begin
+  server.start
+  gets
+rescue
+  puts "an error occurred. exiting."
+  server.stop
+ensure
+  puts "stopping Aspen server."
+  server.stop
+end
