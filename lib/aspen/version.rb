c26672d3f6c4c94310e2bb6d75b22de59406b07e
Kevin Williams <kevwil@gmail.com>
2009-12-05 09:56:26 -0700

    adding some Yard doc to VERSION

--- lib/aspen/version.rb
+++ lib/aspen/version.rb
@@ -1,16 +1,26 @@
 module Aspen
   
-  module VERSION #:nodoc:
+  # @author Kevin Williams
+  # @since 1.0.0
+  # @version 1.0.0
+  module VERSION
+    # major version
     MAJOR    = 1
+    # minor version
     MINOR    = 0
+    # bugfix version
     TINY     = 0
 
+    # version digits joined as a string
     STRING   = [MAJOR, MINOR, TINY].join('.')
 
-    RACK     = [1, 0].freeze # Rack protocol version
+    # Rack protocol version
+    RACK     = [1, 0].freeze
   end
 
+  # name string
   NAME    = 'aspen'.freeze
+  # server name and version
   SERVER  = "#{NAME} #{VERSION::STRING}".freeze
   
 end
