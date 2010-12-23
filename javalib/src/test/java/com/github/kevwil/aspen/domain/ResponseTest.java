106f6e8b4af7a6be03ca7c2f9971c0f2a6a8a4af
Kevin Williams <kevwil@gmail.com>
2010-12-23 15:21:35 -0700

    fill out Response object

--- javalib/src/test/java/com/github/kevwil/aspen/domain/ResponseTest.java
+++ javalib/src/test/java/com/github/kevwil/aspen/domain/ResponseTest.java
@@ -1,8 +1,9 @@
 package com.github.kevwil.aspen.domain;
 
+import org.jboss.netty.handler.codec.http.*;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 /**
  * @author kevwil
@@ -10,9 +11,18 @@ import static org.junit.Assert.assertTrue;
  */
 public class ResponseTest
 {
+    private Request req;
+    private Response resp;
+
     @Test
     public void shouldDoNothing()
     {
-        assertTrue( true );
+        req = new Request( new DefaultHttpRequest( HttpVersion.HTTP_1_1, HttpMethod.GET, "/" ) );
+        resp = new Response( req );
+        assertNotNull( resp );
+        assertNull( resp.getBody() );
+        assertNull( resp.getException() );
+        assertTrue( resp.getHeaderNames().isEmpty() );
+        assertEquals( req, resp.getRequest() );
     }
 }
