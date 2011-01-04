ac50d3053f72bed38ddc821c9768e2bcdcb91ec4
Kevin Williams <kevwil@gmail.com>
2011-01-04 16:30:39 -0700

    update javadoc comments to reflect latest Netty syntax

--- javalib/src/main/java/com/github/kevwil/aspen/MultipleInstanceHandler.java
+++ javalib/src/main/java/com/github/kevwil/aspen/MultipleInstanceHandler.java
@@ -5,7 +5,7 @@ import org.jboss.netty.channel.ChannelHandler;
 /**
  * Non-thread-safe Netty callback handler
  *
- * "one" means you must create a new instance of
+ * "sharable" means you must create a new instance of
  * this handler type for each new channel. It means
  * the member variables of the handler instance can not be
  * shared at all, and violating this contract will
