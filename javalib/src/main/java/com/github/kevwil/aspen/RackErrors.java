1df6921663eaa286203a582a0423951337308267
Kevin Williams <kevwil@gmail.com>
2011-02-03 22:21:00 -0700

    implement RackErrors interface as a $stderr wrapper

--- javalib/src/main/java/com/github/kevwil/aspen/RackErrors.java
+++ javalib/src/main/java/com/github/kevwil/aspen/RackErrors.java
@@ -1,9 +1,43 @@
 package com.github.kevwil.aspen;
 
+import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.builtin.IRubyObject;
+
 /**
- * @author sensei
- * @since 02 03, 2011
+ * @author kevwil
+ * @since Feb 03, 2011
  */
 public interface RackErrors
 {
+    /**
+     * puts must be called with a single argument that responds to to_s.
+     *
+     * @param context ruby context
+     * @param arg must respond to to_s
+     * @return nil
+     */
+    IRubyObject puts( ThreadContext context, IRubyObject arg );
+
+    /**
+     * write must be called with a single argument that is a String.
+     *
+     * @param context ruby context
+     * @param string a Ruby String
+     * @return nil
+     */
+    IRubyObject write( ThreadContext context, IRubyObject string );
+
+    /**
+     * flush must be called without arguments and must be called in order to make the error appear for sure.
+     *
+     * @return nil
+     */
+    IRubyObject flush();
+
+    /**
+     * close must never be called on the error stream.
+     *
+     * @return nil
+     */
+    IRubyObject close();
 }
--- javalib/src/main/java/com/github/kevwil/aspen/io/RubyIORackErrors.java
+++ javalib/src/main/java/com/github/kevwil/aspen/io/RubyIORackErrors.java
@@ -1,9 +1,84 @@
 package com.github.kevwil.aspen.io;
 
+import com.github.kevwil.aspen.RackErrors;
+import org.jruby.*;
+import org.jruby.runtime.ObjectAllocator;
+import org.jruby.runtime.ThreadContext;
+import org.jruby.runtime.builtin.IRubyObject;
+
 /**
- * @author sensei
- * @since 02 03, 2011
+ * @author kevwil
+ * @since Feb 03, 2011
  */
 public class RubyIORackErrors
+extends RubyObject
+implements RackErrors
 {
+    private static final ObjectAllocator ALLOCATOR = new ObjectAllocator()
+    {
+        public IRubyObject allocate( Ruby runtime, RubyClass klass )
+        {
+            return new RubyIORackErrors( runtime, klass );
+        }
+    };
+
+    public static RubyClass getClass(
+            Ruby runtime,
+            String name,
+            RubyClass parent,
+            ObjectAllocator allocator,
+            Class annoClass)
+    {
+        RubyModule jrubyMod = runtime.getOrCreateModule( "Aspen" );
+        RubyClass klass = jrubyMod.getClass( name );
+        if( klass == null )
+        {
+            klass = jrubyMod.defineClassUnder( name, parent, allocator );
+            klass.defineAnnotatedMethods( annoClass );
+        }
+        return klass;
+    }
+
+    public static RubyClass getRubyIORackErrorsClass( Ruby runtime )
+    {
+        return getClass( runtime, "RubyIORackErrors", runtime.getObject(),
+                ALLOCATOR, RubyIORackErrors.class );
+    }
+
+    public RubyIORackErrors( Ruby runtime, RubyClass metaClass )
+    {
+        super( runtime, metaClass );
+        _stderr = (RubyIO) runtime.getGlobalVariables().get( "$stderr" );
+    }
+
+    public RubyIORackErrors( Ruby runtime )
+    {
+        super( runtime, getRubyIORackErrorsClass( runtime ) );
+    }
+
+    private RubyIO _stderr;
+
+    @Override
+    public IRubyObject puts( final ThreadContext context, final IRubyObject arg )
+    {
+        return _stderr.puts( context, new IRubyObject[]{ arg } );
+    }
+
+    @Override
+    public IRubyObject write( final ThreadContext context, final IRubyObject string )
+    {
+        return _stderr.write( context, string );
+    }
+
+    @Override
+    public IRubyObject flush()
+    {
+        return _stderr.flush();
+    }
+
+    @Override
+    public IRubyObject close()
+    {
+        throw _stderr.getRuntime().newIOError( "Rack spec prohibits calling close() on rack.errors stream." );
+    }
 }
