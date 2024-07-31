ffd633647cd4e3a731ed5b9677f9c3a6b581d98e
Kevin Williams <kevwil@users.noreply.github.com>
2024-07-30 22:40:45 -0600

    EmbeddedChannel for testing

--- javalib/src/test/java/com/github/kevwil/aspen/RackChannelInboundHandlerTest.java
+++ javalib/src/test/java/com/github/kevwil/aspen/RackChannelInboundHandlerTest.java
@@ -2,14 +2,13 @@ package com.github.kevwil.aspen;
 
 import com.github.kevwil.aspen.domain.Request;
 import com.github.kevwil.aspen.domain.Response;
-import io.netty.channel.*;
-import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.http.*;
 import org.jruby.Ruby;
 import org.junit.Test;
 
 import static org.easymock.EasyMock.*;
+import static org.junit.Assert.assertTrue;
 
 /**
  * @author kevwil
@@ -18,38 +17,38 @@ import static org.easymock.EasyMock.*;
 public class RackChannelInboundHandlerTest
 {
     @Test
-    public void shouldPassRequestToProxyAndReceiveResponse() throws Exception
+    public void shouldPassRequestToProxyAndReceiveResponse()
     {
-        EventLoopGroup group = null;
-        try {
-            RackProxy rack = createMock(RackProxy.class);
-            group = new NioEventLoopGroup();
-            Channel channel = new ReflectiveChannelFactory<>(NioServerSocketChannel.class).newChannel();
-            group.register(channel);
-            ChannelPipeline pipeline = channel.pipeline();
-            RackChannelInboundHandler handler = new RackChannelInboundHandler(rack);
-            pipeline.addLast("handler", handler);
-            FullHttpRequest hr = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "http://localhost/");
-            ChannelHandlerContext ctx = RackUtil.buildDummyChannelHandlerContext("localhost", "80");
-            Request request = createMockBuilder(Request.class).withConstructor(ctx, hr, Ruby.getGlobalRuntime()).createMock();
-            Response response = new Response(request);
+        Ruby rubyRuntime = Ruby.getGlobalRuntime();
+        // mocks
+        RackProxy rack = createMock(RackProxy.class);
+        Request request = createMockBuilder(Request.class)
+                .withConstructor(
+                        RackUtil.buildDummyChannelHandlerContext("localhost", "80"),
+                        new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "http://localhost/"),
+                        rubyRuntime
+                ).createMock();
 
-            expect(rack.getRuntime()).andReturn(Ruby.getGlobalRuntime());
-            expect(rack.process(anyObject(Request.class))).andReturn(response);
-            replay(request);
-            replay(rack);
+        // netty setup
+        EmbeddedChannel channel = new EmbeddedChannel();
 
-            pipeline.fireChannelRead(hr);
+        // test object setup
+        RackChannelInboundHandler handler = new RackChannelInboundHandler(rack);
+        channel.pipeline().addLast("rack", handler);
 
-            verify(request);
-            verify(rack);
-        }
-        finally
-        {
-            if(group != null) {
-                group.shutdownGracefully();
-                group.terminationFuture().sync();
-            }
-        }
+        // test
+        Response response = new Response(request);
+        expect(rack.getRuntime()).andReturn(rubyRuntime).anyTimes();
+        expect(rack.process(anyObject(Request.class))).andReturn(response);
+
+        replay(request);
+        replay(rack);
+
+        assertTrue(channel.writeInbound(request.getHttpRequest()));
+
+        verify(request);
+        verify(rack);
+
+        channel.finishAndReleaseAll();
     }
 }
